c854c70e5fea98c7e065db3ab782fb11e7abaa19
Marshal Wu <marshal.wu@gmail.com>
2022-06-14 17:26:08 +0800

    update

--- packages/app/client/src/pages/extends/blocks/remote-data-block/RemoteData.tsx
+++ packages/app/client/src/pages/extends/blocks/remote-data-block/RemoteData.tsx
@@ -1,7 +1,14 @@
-import { useField } from '@formily/react';
+import { useField, ISchema, useFieldSchema } from '@formily/react';
 import { useTranslation } from 'react-i18next';
-import { GeneralSchemaDesigner, SchemaSettings, APIClient, APIClientProvider, useRequest } from '@nocobase/client';
-import { Spin,Button } from 'antd';
+import {
+  GeneralSchemaDesigner,
+  SchemaSettings,
+  APIClient,
+  APIClientProvider,
+  useRequest,
+  useDesignable,
+} from '@nocobase/client';
+import { Spin, Button } from 'antd';
 import { useState } from 'react';
 
 export const RemoteData = () => {
@@ -10,6 +17,7 @@ export const RemoteData = () => {
   });
 
   const [count, setCount] = useState(false);
+  const [title, setTitile] = useState('获取远程数据');
 
   const DataControl = () => {
     const refreshAction = () => {
@@ -18,13 +26,16 @@ export const RemoteData = () => {
 
     return (
       <div>
-        <div><Button type="primary" onClick={refreshAction}>刷新</Button></div>
+        <div>
+          <Button type="primary" onClick={refreshAction}>
+            刷新
+          </Button>
+        </div>
         <DataItem />
       </div>
     );
   };
 
-  // @ts-ignore
   const DataItem = () => {
     const { data, loading } = useRequest({ url: '/fact' });
 
@@ -41,15 +52,48 @@ export const RemoteData = () => {
 
   return (
     <APIClientProvider apiClient={apiClient}>
-      <h3>获取远程数据</h3>
+      <h3>{title}</h3>
       <DataControl />
     </APIClientProvider>
   );
 };
 
 RemoteData.Designer = () => {
+  const { dn } = useDesignable();
+  const fieldSchema = useFieldSchema();
+  const field = useField();
+
   return (
     <GeneralSchemaDesigner>
+      <SchemaSettings.ModalItem
+        title={'设置参数'}
+        schema={
+          {
+            type: 'object',
+            title: '设置数据范围',
+            properties: {
+              title: {
+                type: 'string',
+                title: '标题',
+                'x-decorator': 'FormItem',
+                'x-component': 'Input',
+                default: 'title',
+              },
+            },
+          } as ISchema
+        }
+        // @ts-ignore
+        onSubmit={({ title }) => {
+          dn.emit('patch', {
+            schema: {
+              ['x-uid']: fieldSchema['x-uid'],
+              'x-decorator-props': field.decoratorProps,
+            },
+          });
+        }}
+      />
+      {/* @ts-ignore */}
+      <SchemaSettings.Divider />
       {/* @ts-ignore */}
       <SchemaSettings.Remove
         removeParentsIfNoChildren
